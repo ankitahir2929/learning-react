9b04682524c9a5bbcf3a749f67e0f69b43d2881a
Pithiya Ankit <pithiyaankit71@gmail.com>
2025-12-29 16:56:22 +0530

    learning css

---
+++ 04.CardsProject/src/components/Card.jsx
@@ -0,0 +1,37 @@
+import { Bookmark } from "lucide-react";
+
+const Card = (props) => {
+  return (
+    <div className="card">
+      <div>
+        <div className="top">
+          <img src={props.img} />
+          <button>
+            Save <Bookmark size={14} />
+          </button>
+        </div>
+
+        <div className="center">
+          <h3>
+            {props.name} <span>{props.date}</span>
+          </h3>
+          <h2>{props.post}</h2>
+          <div className="tag">
+            <h4>{props.tag1}</h4>
+            <h4>{props.tag2}</h4>
+          </div>
+        </div>
+      </div>
+
+      <div className="bottom">
+        <div>
+          <h3>$ {props.salary}</h3>
+          <p>{props.location}</p>
+        </div>
+        <button>Apply Now</button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
